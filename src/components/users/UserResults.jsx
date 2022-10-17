bcd877512ec29e711ef37984ce215c094d3587b0
Snezhana <snezhana.stoyanova@icb.bg>
2022-10-17 15:39:09 +0300

    50. UserList Component

---
+++ src/components/users/UserResults.jsx
@@ -0,0 +1,34 @@
+import {useEffect, useState} from 'react'
+
+function UserResults() {
+    const [users, setUsers] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        fetchUsers()
+    }, [])
+    
+    const fetchUsers = async () => {
+        const response = await fetch(`https://api.github.com/users`)
+
+        const data = await response.json()
+
+        console.log(data)
+        setUsers(data)
+        setLoading(false)
+    }
+
+    if(!loading){
+        return ( <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
+        {users.map((user)=>{
+            return <h3 key={user.login}>{user.login}</h3>
+         })}
+         </div>
+       )
+    }else{
+        return <h3>Loading....</h3>
+    }
+  
+}
+
+export default UserResults;
