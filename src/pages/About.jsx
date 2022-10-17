db8392d029ed122b90fc7404ee12c1c01aaf4f11
Snezhana <snezhana.stoyanova@icb.bg>
2022-10-17 14:14:43 +0300

    48....

---
+++ src/pages/About.jsx
@@ -0,0 +1,19 @@
+function About() {
+  return (
+    <>
+      <h1 className="text-6xl mb-4">Github Finder</h1>
+      <p className="mb-4 text-2xl font-light">
+        A React app to search GitHub profiles and see profile details. 
+      </p>
+      <p className="text-lg text-gray-400">
+        Version <span>1.0.0</span>
+      </p>
+      <p className="text-lg text-gray-400">
+        By:
+        <a href='/'>Snejana Stoyanova</a>
+      </p>
+    </>
+  );
+}
+
+export default About;
