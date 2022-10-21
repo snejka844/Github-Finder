e63181151f874fffe19ffc6cd835795e3a5ac540
Snezhana <snezhana.stoyanova@icb.bg>
2022-10-21 11:33:04 +0300

    59. Alert Context & Reducer 60. Alert Component 61. Get Single User 62. User Profile Top 63. User Profile Stats 64. Get User Repos 65. Repo Items

--- src/context/github/GithubContext.jsx
+++ src/context/github/GithubContext.jsx
@@ -8,6 +8,8 @@ const GITHUB_URL = "https://api.github.com";
 export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
+    user:{},
+    repos:[],
     loading: false,
   };
 
@@ -40,6 +42,43 @@ const searchUsers = async (text) => {
     });
   };
 
+  
+const getUserRepos = async (login) => {
+  setLoading();
+
+  const params =new URLSearchParams({
+    sort: 'created',
+    per_page: 10,
+})
+
+  const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`);
+
+  const data = await response.json();
+
+  dispatch({
+    type: "GET_REPOS",
+    payload: data,
+  });
+};
+
+  const getUser = async (login) => {
+    setLoading();
+  
+    const response = await fetch(`${GITHUB_URL}/users/${login}`);
+
+    if(response.status===404){
+      window.location = '/notfound';
+    }else{
+      const data = await response.json();
+
+      dispatch({
+        type: "GET_USER",
+        payload: data,
+      });
+    }
+  
+  };
+
   const clearUsers =  () => {
 
     dispatch({
@@ -51,7 +90,16 @@ const searchUsers = async (text) => {
 
   return (
     <GithubContext.Provider
-      value={{ users: state.users, loading: state.loading, searchUsers,clearUsers }}
+      value={{ 
+        users: state.users, 
+        user:state.user,
+        loading: state.loading, 
+        repos: state.repos,
+        searchUsers,
+        clearUsers,
+        getUser,
+        getUserRepos,
+       }}
     >
       {children}
     </GithubContext.Provider>
